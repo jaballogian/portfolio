d6bea0404a467b0891ca5dfa9bdd662118b18c50
jaballogian <jaballogianrma77@gmail.com>
2021-09-02 22:37:57 +0700

    add colors util

---
+++ src/utils/colors.js
@@ -0,0 +1,14 @@
+const colors = {
+  blueNCS: '#118AB2', // BLUE
+  cadetBlueCrayola: '#ADB5BD', // GRAY
+  carribeanGreen: '#06D6A0', // GREEN
+  cultured: '#E9ECEF', // LIGHT GRAY
+  orangeYellowCrayota: '#FFD166', // YELLOW
+  paradisePink: '#EF476F', // RED
+  slateGray: '#6C757D', // DARK GRAY
+  stackoverflowGold: '#FFCC01', // GOLD
+  stackoverflowSilver: '#B4B8BC', // SILVER
+  stackoverflowBronze: '#D1A684', // BRONZE
+}
+
+export default colors
