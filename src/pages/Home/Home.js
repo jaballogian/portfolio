73021721dfbd8dc66c2af121ce1cb7d35928e3ea
jaballogian <jaballogianrma77@gmail.com>
2021-09-02 21:39:03 +0700

    add Home page and its styles

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+// STYLES
+import useStyles from './homeUseStyles'
+
+const Home = () => {
+  const classes = useStyles()
+
+  return (
+    <div className={classes['root']}>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/pages/Home/homeUseStyles.js
@@ -0,0 +1,12 @@
+// MATERIAL UI
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    width: '100%',
+    boxSizing: 'border-box',
+  },
+}))
+
+export default useStyles
