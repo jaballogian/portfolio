8f8d7d877971a9becccd6d4ccef3add8df29b618
jaballogian <jaballogianrma77@gmail.com>
2021-09-04 17:29:47 +0700

    add Hard Skill component in Home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 // COMPONENTS (SORTED BY ORDER)
 import Introduction from './Introduction/Introduction'
+import HardSkill from './HardSkill/HardSkill'
 
 // STYLES
 import useStyles from './homeUseStyles'
@@ -12,6 +13,7 @@ const Home = () => {
   return (
     <div className={classes['root']}>
       <Introduction/>
+      <HardSkill/>
     </div>
   )
 }
