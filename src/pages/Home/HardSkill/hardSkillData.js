5a521f0d38fafa296cfd9c5fd32e61ee5f3d24b3
jaballogian <jaballogianrma77@gmail.com>
2021-09-04 17:29:20 +0700

    add hard skill data

---
+++ src/pages/Home/HardSkill/hardSkillData.js
@@ -0,0 +1,36 @@
+const hardSkillList = [
+  {
+    title: 'Technology',
+    contents: [
+      'Android Studio',
+      'React Js',
+      'React Native',
+      'Firebase',
+      'Arduino IDE',
+    ]
+  },
+  {
+    title: 'Languages',
+    contents: [
+      'HTML',
+      'CSS',
+      'Javascript',
+      'Java',
+      'Python',
+      'C++',
+    ],
+  },
+  {
+    title: 'Tools',
+    contents: [
+      'Git',
+      'Notion',
+      'Trello',
+      'Jira',
+      'Figma',
+      'Easy Eda',
+    ],
+  },
+]
+
+export default hardSkillList
