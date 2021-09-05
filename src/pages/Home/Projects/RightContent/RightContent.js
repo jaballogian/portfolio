2ce59dce64e37360d2ae2fc0d72190ed06b9352c
jaballogian <jaballogianrma77@gmail.com>
2021-09-05 19:16:24 +0700

    add Right Content component and its styles

---
+++ src/pages/Home/Projects/RightContent/RightContent.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+// MATERIAL UI CORES
+import Grid from '@material-ui/core/Grid'
+
+// STYELS
+import useStyles from './rightContentUseStyles'
+
+const RightContent = () => {
+  const classes = useStyles()
+
+  return (
+    <Grid 
+      item 
+      xs={6}
+    >
+      <div className={classes['root']}>
+      </div>
+    </Grid>
+  )
+}
+
+export default RightContent
