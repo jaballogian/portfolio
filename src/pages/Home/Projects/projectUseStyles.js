b8f545af222b3bfa4834bf93ffde7ca8f6638df2
jaballogian <jaballogianrma77@gmail.com>
2021-09-05 20:34:19 +0700

    delete unused code in Projects component

--- src/pages/Home/Projects/projectUseStyles.js
+++ src/pages/Home/Projects/projectUseStyles.js
@@ -1,9 +1,6 @@
 // MATERIAL UI
 import { makeStyles } from '@material-ui/core/styles'
 
-// UTILS
-import colors from '../../../utils/colors'
-
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
