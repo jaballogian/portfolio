56831450ff28c1454a607a1a7e8e4a69d924f2f6
jaballogian <jaballogianrma77@gmail.com>
2021-09-05 19:16:43 +0700

    add Projects component and its styles

---
+++ src/pages/Home/Projects/Projects.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+// COMPONENTS (SORTED BY ORDER)
+import LeftContent from './LeftContent/LeftContent'
+import RightContent from './RightContent/RightContent'
+
+// MATERIAL UI CORES
+import Grid from '@material-ui/core/Grid'
+
+// STYLES
+import useStyles from './projectUseStyles'
+
+const Projects = () => {
+  const classes = useStyles()
+
+  return (
+    <div className={classes['root']}>
+      <Grid container>
+        <LeftContent/>
+        <RightContent/>
+      </Grid>
+    </div>
+  )
+}
+
+export default Projects
---
+++ src/pages/Home/Projects/projectUseStyles.js
@@ -0,0 +1,16 @@
+// MATERIAL UI
+import { makeStyles } from '@material-ui/core/styles'
+
+// UTILS
+import colors from '../../../utils/colors'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    width: '100%',
+    height: '100vh',
+    boxSizing: 'border-box',
+  },
+}))
+
+export default useStyles
