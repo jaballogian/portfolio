61d975ee332f56820210f65927c683b9a4f0a32f
jaballogian <jaballogianrma77@gmail.com>
2021-09-07 22:03:29 +0700

    add Page Number Context

---
+++ src/contexts/PageNumberContext.js
@@ -0,0 +1,24 @@
+import React, { createContext, useState } from 'react'
+
+const PageNumberContext = createContext()
+
+const PageNumberContextProvider = (props) => {
+  const [ currentPage, setCurrentPage ] = useState(1)
+
+  const changeCurrentPage = (inputNumber) => {
+    setCurrentPage(inputNumber)
+  }
+
+  return (
+    <PageNumberContext.Provider
+      value={{
+        currentPage,
+        changeCurrentPage,
+      }}
+    >
+      {props['children']}
+    </PageNumberContext.Provider>
+  )
+}
+
+export { PageNumberContextProvider, PageNumberContext }
