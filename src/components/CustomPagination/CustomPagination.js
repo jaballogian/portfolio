af297c87a1c1581e6feba4d88f922e1721b2a881
jaballogian <jaballogianrma77@gmail.com>
2021-09-10 22:34:56 +0700

    add current page and set current page props in Custom Pagination component

--- src/components/CustomPagination/CustomPagination.js
+++ src/components/CustomPagination/CustomPagination.js
@@ -6,7 +6,9 @@ import Pagination from '@material-ui/lab/Pagination'
 // STYLES
 import useStyles from './customPaginationUseStyles'
 
-const CustomPagination = () => {
+const CustomPagination = (props) => {
+  const { currentPage, setCurrentPage } = props
+
   const classes = useStyles()
   
   return (
@@ -15,6 +17,8 @@ const CustomPagination = () => {
         count={10}
         siblingCount={0}
         boundaryCount={1}
+        page={currentPage}
+        onChange={(event, value) => setCurrentPage(value)}
       />
     </div>
   )
