7c89621054bc0c95118522395cb10a32ee2423b2
jaballogian <jaballogianrma77@gmail.com>
2021-09-11 17:46:23 +0700

    change website with desktop word and add some comments in projects data

--- src/pages/Home/Projects/projectsData.js
+++ src/pages/Home/Projects/projectsData.js
@@ -1,26 +1,32 @@
 const projectsData = [
   {
+    // LEFT CONTENT
     title: 'Project Title 1',
     type: 'Mobile App',
     technologies: 'Android Studio, Arudino IDE, Firebase',
     description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
     link: '/project/1',
+    // RIGHT CONTENT
     imageType: 'mobile',
   },
   {
+    // LEFT CONTENT
     title: 'Project Title 2',
     type: 'Website',
     technologies: 'Android Studio, Arudino IDE, Firebase',
     description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
     link: '/project/2',
-    imageType: 'website',
+    // RIGHT CONTENT
+    imageType: 'desktop',
   },
   {
+    // LEFT CONTENT
     title: 'Project Title 3',
     type: 'IoT',
     technologies: 'Android Studio, Arudino IDE, Firebase',
     description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
     link: '/project/3',
+    // RIGHT CONTENT
     imageType: 'other',
   },
 ]
