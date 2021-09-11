6c4fecefcf0de46ebd74000f884c9fbb702f986b
jaballogian <jaballogianrma77@gmail.com>
2021-09-11 22:02:23 +0700

    change the style of Projects component

--- src/pages/Home/Projects/projectUseStyles.js
+++ src/pages/Home/Projects/projectUseStyles.js
@@ -6,12 +6,20 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1,
     width: '100%',
     height: '100vh',
+    minHeight: 800,
     boxSizing: 'border-box',
+    display: 'flex',
+    alignItems: 'center',
+    position: 'relative',
   },
   paginationContainer: {
     display: 'flex',
     justifyContent: 'center',
-    transform: 'translate(0px, -75px)',
+    position: 'absolute',
+    left: '50%',
+    transform: 'translate(-50%, 0px)',
+    bottom: 25,
+    zIndex: 1,
   },
 }))
 
