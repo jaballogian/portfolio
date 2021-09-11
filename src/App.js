a038bb8eacc05e079acd1e103e73026b78d7c8e6
jaballogian <jaballogianrma77@gmail.com>
2021-09-11 09:11:06 +0700

    wrap Home page with Browser Router, Switch, and Route component in App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 // PAGES
 import Home from './pages/Home/Home'
 
 function App() {
   return (
-    <Home/>
+    <Router>
+      <Switch>
+        <Route path='/' exact>
+          <Home/>
+        </Route>
+      </Switch>
+    </Router>
   )
 }
 
