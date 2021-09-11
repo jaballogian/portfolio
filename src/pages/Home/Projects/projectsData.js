c02e6db9f47675abb82863fdc2d3d3a09513d35d
jaballogian <jaballogianrma77@gmail.com>
2021-09-11 09:01:36 +0700

    add projects data for Projects page

---
+++ src/pages/Home/Projects/projectsData.js
@@ -0,0 +1,28 @@
+const projectsData = [
+  {
+    title: 'Project Title 1',
+    type: 'Mobile App',
+    technologies: 'Android Studio, Arudino IDE, Firebase',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    link: '/project/1',
+    imageType: 'mobile',
+  },
+  {
+    title: 'Project Title 2',
+    type: 'Website',
+    technologies: 'Android Studio, Arudino IDE, Firebase',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    link: '/project/2',
+    imageType: 'website',
+  },
+  {
+    title: 'Project Title 3',
+    type: 'IoT',
+    technologies: 'Android Studio, Arudino IDE, Firebase',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    link: '/project/3',
+    imageType: 'other',
+  },
+]
+
+export default projectsData
