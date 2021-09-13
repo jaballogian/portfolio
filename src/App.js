af0c04c584b69c909b8607cda056939b30315ad2
jaballogian <jaballogianrma77@gmail.com>
2021-09-13 20:50:33 +0700

    change how to print all pages in App component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,36 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 // PAGES
 import Home from './pages/Home/Home'
+import BeingDeveloped from './pages/BeingDeveloped/BeingDeveloped'
 
 function App() {
+  const pageList = [
+    // HOME PAGE
+    {
+      path: '/',
+      exact: true,
+      component: <Home/>,
+    },
+    // PROJECT DETAIL PAGE
+    {
+      path: '/project/:projectId',
+      exact: true,
+      component: <BeingDeveloped/>,
+    },
+  ]
+
   return (
     <Router>
       <Switch>
-        <Route path='/' exact>
-          <Home/>
-        </Route>
+        {pageList.map((item, index) => (
+          <Route 
+            key={index} 
+            path={item['path']} 
+            exact={item['exact']}
+          >
+            {item['component']}
+          </Route>
+        ))}
       </Switch>
     </Router>
   )
