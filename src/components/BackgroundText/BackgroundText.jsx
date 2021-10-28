1ead6b0757f468fbb0365b4e68e455b63053381e
jaballogian <jaballogianrma77@gmail.com>
2021-10-29 06:44:28 +0700

    add some props in Background Text component

--- src/components/BackgroundText/BackgroundText.jsx
+++ src/components/BackgroundText/BackgroundText.jsx
@@ -7,26 +7,46 @@ import Typography from '@mui/material/Typography'
 // STYLES
 import useStyles from './backgroundTextUseStyles'
 
+const textVariantList = [
+  'h1', 
+  'h2', 
+  'h3', 
+  'h4', 
+  'h5', 
+  'subtitle1', 
+  'subtitle2',
+]
+
 const BackgroundText = (props) => {
-  const { text } = props
+  const { 
+    text, 
+    className,
+    noWrap,
+    textVariant,
+  } = props
 
   const classes = useStyles()
 
   return (
     <Typography 
-      variant='h1'
-      noWrap
-      className={classes.backgroundText}
+      variant={textVariant}
+      noWrap={noWrap}
+      className={`${classes.backgroundText} ${className}`}
     >
       {text}
     </Typography>
   )
 }
 
-BackgroundText.defaultProps = {}
+BackgroundText.defaultProps = {
+  noWrap: true,
+}
 
 BackgroundText.propTypes = {
   text: PropTypes.string.isRequired,
+  className: PropTypes.string,
+  noWrap: PropTypes.bool,
+  textVariant: PropTypes.oneOf(textVariantList).isRequired,
 }
 
 export default BackgroundText
