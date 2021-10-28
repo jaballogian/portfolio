7d5933bfa6b5db58037a9d8935bde661f0fcd612
jaballogian <jaballogianrma77@gmail.com>
2021-10-29 06:44:42 +0700

    change the styles of Background Text component

--- src/components/BackgroundText/backgroundTextUseStyles.js
+++ src/components/BackgroundText/backgroundTextUseStyles.js
@@ -14,8 +14,6 @@ const useStyles = makeStyles((theme) => ({
     bottom: 0,
     left: '50%',
     transform: 'translateX(-50%)',
-    height: 152.5,
-    fontSize: 175,
     overflow: 'hidden',
     zIndex: -1,
     lineHeight: 1,
