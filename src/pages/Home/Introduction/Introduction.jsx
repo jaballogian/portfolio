ffacf69a6f11fbad0c8faf0d387365cf1435b96d
jaballogian <jaballogianrma77@gmail.com>
2021-10-29 06:04:54 +0700

    replace all Box components with div elements in Introduction component

--- src/pages/Home/Introduction/Introduction.jsx
+++ src/pages/Home/Introduction/Introduction.jsx
@@ -7,7 +7,6 @@ import BackgroundText from 'components/BackgroundText/BackgroundText'
 import IllustrationsPerson from 'assets/images/illustrations/introduction_person.svg'
 
 // MATERIAL UI CORES
-import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
 
 // STYLES
@@ -17,16 +16,16 @@ const Introduction = () => {
   const classes = useStyles()
 
   return (
-    <Box className={classes.introdutionRoot}>
+    <div className={classes.introdutionRoot}>
       {/* LEFT SECTION */}
-      <Box className={classes.section}>
-        <Box className={classes.titleAndCaption}>
+      <div className={classes.section}>
+        <div className={classes.titleAndCaption}>
           {/* TITLE */}
           <Typography 
             variant='h1'
             className={classes.title}
           >
-            Hello, I'm <Box className={classes.blueText}>Jabal</Box>
+            Hello, I'm <div className={classes.blueText}>Jabal</div>
           </Typography>
 
           {/* CAPTION */}
@@ -38,22 +37,21 @@ const Introduction = () => {
             <br/>mobile application developer,
             <br/>and IoT engineer.
           </Typography>
-        </Box>
-      </Box>
+        </div>
+      </div>
 
       {/* RIGHT SECTION */}
-      <Box className={classes.section}>
-        <Box 
-          component='img'
+      <div className={`${classes.section} ${classes.illustrationSection}`}>
+        <img
           src={IllustrationsPerson}
           alt=''
           className={classes.illustration}
         />
-      </Box>
+      </div>
 
       {/* BACKGROUND TEXT */}
       <BackgroundText text='SOFTWARE ENGINEER'/>
-    </Box>
+    </div>
   )
 }
 
