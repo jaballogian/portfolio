549bb549753947c4c9709f0c2aaa394016632c95
jaballogian <jaballogianrma77@gmail.com>
2021-10-30 06:19:57 +0700

    add Contacts component and its styles

---
+++ src/components/Contacts/Contacts.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+// STYLES
+import useStyles from './contactsUseStyles'
+
+const Contacts = () => {
+  const classes = useStyles()
+  
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Contacts
---
+++ src/components/Contacts/contactsUseStyles.js
@@ -0,0 +1,10 @@
+// CONSTANTS
+import colors from 'constants/colors'
+
+// MATERIAL UI STYLES
+import { makeStyles } from '@mui/styles'
+
+const useStyles = makeStyles((theme) => ({
+}))
+
+export default useStyles
