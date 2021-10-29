6ee5534b71961489b8168aad4e2fb4ada1ad87df
jaballogian <jaballogianrma77@gmail.com>
2021-10-30 06:13:45 +0700

    implement use location to grab the hash address value and add section class name function to create the class name for the section Typography component in Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 
 // MATERIAL UI CORES
 import Typography from '@mui/material/Typography'
@@ -9,6 +10,10 @@ import useStyles from './headerUseStyles'
 const Header = () => {
   const classes = useStyles()
 
+  let location = useLocation()
+  location = location.hash.replace('#', '')
+  console.log(location)
+
   const sectionList = [
     'HOME',
     'SKILLS',
@@ -16,13 +21,22 @@ const Header = () => {
     'SERTIFICATES',
   ]
 
+  const sectionClassName = (inputItem) => {
+    if(inputItem.toLowerCase() === location.toLowerCase()) {
+      return `${classes.sectionText} ${classes.sectionActive}`
+    }
+    else {
+      return classes.sectionText
+    }
+  }
+
   return (
     <div className={classes.headerRoot}>
       {sectionList.map((item, index) => (
         <Typography
           key={index}
           variant='subtitle1'
-          className={`${classes.sectionText} ${classes.sectionActive}`}
+          className={sectionClassName(item)}
         >
           {item}
         </Typography>
