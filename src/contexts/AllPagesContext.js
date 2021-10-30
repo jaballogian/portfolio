7de980edb9f1a5e61df3485d3d5c05cbb8abc3f5
jaballogian <jaballogianrma77@gmail.com>
2021-10-30 21:43:02 +0700

    add breakpint type value in All Pages Context

--- src/contexts/AllPagesContext.js
+++ src/contexts/AllPagesContext.js
@@ -1,8 +1,17 @@
 import React, { createContext, useState } from 'react'
 
+// MATERIAL UI CORES
+import useMediaQuery from '@mui/material/useMediaQuery'
+
 const AllPagesContext = createContext()
 
 const AllPagesContextProvider = (props) => {
+  const isXsScreen = useMediaQuery((theme) => theme.breakpoints.only('xs'))
+  const isSmScreen = useMediaQuery((theme) => theme.breakpoints.only('sm'))
+  const isMdScreen = useMediaQuery((theme) => theme.breakpoints.only('md'))
+  const isLgScreen = useMediaQuery((theme) => theme.breakpoints.only('lg'))
+  const isXlScreen = useMediaQuery((theme) => theme.breakpoints.only('xl'))
+
   // TYPE = OBJECT
   const [ toast, setToast ] = useState(null)
 
@@ -10,10 +19,18 @@ const AllPagesContextProvider = (props) => {
     setToast(inputValue)
   }
 
+  let breakpointType
+  isXsScreen && (breakpointType = 'xs')
+  isSmScreen && (breakpointType = 'sm')
+  isMdScreen && (breakpointType = 'md')
+  isLgScreen && (breakpointType = 'lg')
+  isXlScreen && (breakpointType = 'xl')
+
   return (
     <AllPagesContext.Provider
       value={{
         toast, changeToast,
+        breakpointType,
       }}
     >
       {props['children']}
