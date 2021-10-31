02f7457f8bb898e689fe07dcd23684c259857a2f
jaballogian <jaballogianrma77@gmail.com>
2021-10-31 21:34:36 +0700

    add Header Contacts Controller component and its styles

---
+++ src/components/HeaderContactsController/HeaderContactsController.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+// STYLES
+import useStyles from './headerContactsControllerUseStyles'
+
+const HeaderContactsController = () => {
+  const classes = useStyles()
+  
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default HeaderContactsController
---
+++ src/components/HeaderContactsController/headerContactsControllerUseStyles.js
@@ -0,0 +1,9 @@
+// MATERIAL UI STYLES
+import { makeStyles } from '@mui/styles'
+
+const useStyles = makeStyles((theme) => ({
+  headerContactsControllerRoot: {
+  },
+}))
+
+export default useStyles
