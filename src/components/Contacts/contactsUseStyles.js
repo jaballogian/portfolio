6800e0334a3c314e633be34f82b2503a5cd460bc
jaballogian <jaballogianrma77@gmail.com>
2021-11-02 21:20:00 +0700

    make clear the responsive code for Contacts component

--- src/components/Contacts/contactsUseStyles.js
+++ src/components/Contacts/contactsUseStyles.js
@@ -12,7 +12,10 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: 'column',
     bottom: 100,
     left: 50,
-    [theme.breakpoints.between('sm', 'lg')]: {
+    [theme.breakpoints.only('sm')]: {
+      left: 25,
+    },
+    [theme.breakpoints.only('md')]: {
       left: 25,
     },
   },
