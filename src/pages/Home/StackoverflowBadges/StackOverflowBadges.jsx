28aa1b2e6685616122b3f2bd47f40f70048dd936
jaballogian <jaballogianrma77@gmail.com>
2021-11-03 21:55:28 +0700

    fill contents and their styles in Stackoverflow Badges component

--- src/pages/Home/StackoverflowBadges/StackOverflowBadges.jsx
+++ src/pages/Home/StackoverflowBadges/StackOverflowBadges.jsx
@@ -1,14 +1,54 @@
 import React from 'react'
 
+// CONSTANTS
+import colors from 'constants/colors'
+
+// MATERIAL UI CORES
+import Typography from '@mui/material/Typography'
+
 // STYLES
 import useStyles from './stackOverflowBadgesUseStyles'
 
 const StackOverflowBadges = () => {
   const classes = useStyles()
-  
+
+  const badgesList = [
+    {
+      count: 2,
+      color: colors.stackoverflowGold,
+    },
+    {
+      count: 6,
+      color: colors.stackoverflowSilver,
+    },
+    {
+      count: 20,
+      color: colors.stackoverflowBronze,
+    },
+  ]
+
   return (
-    <div>
-      
+    <div className={classes.stackoverflowBadgesRoot}>
+      {badgesList.map((item, index) => (
+        <div 
+          key={index}
+          className={classes.badgesItemContainer}
+        >
+          {/* CIRCLE */}
+          <div 
+            className={classes.circle}
+            style={{ backgroundColor: item.color }}
+          />
+
+          {/* TEXT */}
+          <Typography 
+            className={classes.text}
+            style={{ color: item.color }}
+          >
+            {item.count}
+          </Typography>
+        </div>
+      ))}
     </div>
   )
 }
--- src/pages/Home/StackoverflowBadges/stackOverflowBadgesUseStyles.js
+++ src/pages/Home/StackoverflowBadges/stackOverflowBadgesUseStyles.js
@@ -1,10 +1,30 @@
-// CONSTANTS
-import colors from 'constants/colors'
-
 // MATERIAL UI STYLES
 import { makeStyles } from '@mui/styles'
 
 const useStyles = makeStyles((theme) => ({
+  stackoverflowBadgesRoot: {
+    position: 'absolute',
+    right: 50,
+    bottom: 100,
+    cursor: 'pointer',
+  },
+  badgesItemContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    marginBottom: 10,
+    '&:last-child': {
+      marginBottom: 0,
+    },
+  },
+  circle: {
+    width: 20,
+    height: 20,
+    borderRadius: '50%',
+    marginRight: 10,
+  },
+  text: {
+    lineHeight: 1,
+  },
 }))
 
 export default useStyles
