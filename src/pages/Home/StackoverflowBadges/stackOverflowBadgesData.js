aa1b9981af1d115bd493c0810a72b8b1d4672b89
jaballogian <jaballogianrma77@gmail.com>
2021-11-10 21:20:41 +0700

    add data for Stackoverflow Badges component

---
+++ src/pages/Home/StackoverflowBadges/stackOverflowBadgesData.js
@@ -0,0 +1,19 @@
+// CONSTANTS
+import colors from 'constants/colors'
+
+const badgesList = [
+  {
+    count: 2,
+    color: colors.stackoverflowGold,
+  },
+  {
+    count: 7,
+    color: colors.stackoverflowSilver,
+  },
+  {
+    count: 20,
+    color: colors.stackoverflowBronze,
+  },
+]
+
+export default badgesList
