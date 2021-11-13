8c270ea36468358dce247a1963da36af416ef0e4
jaballogian <jaballogianrma77@gmail.com>
2021-11-13 22:18:06 +0700

    delete unused code for styles of Introduction component

--- src/pages/Home/Introduction/introductionUseStyles.js
+++ src/pages/Home/Introduction/introductionUseStyles.js
@@ -9,7 +9,6 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1,
     width: '100%',
     height: '100vh',
-    bomdizing: 'border-box',
     display: 'flex',
     position: 'relative',
     [theme.breakpoints.down('md')]: {
