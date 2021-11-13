7f7edbdb9448950de56dc5a167831f77dac2542c
jaballogian <jaballogianrma77@gmail.com>
2021-11-13 22:13:00 +0700

    change the responsive styles of Custom Pagination component

--- src/components/CustomPagination/customPaginationStyles.js
+++ src/components/CustomPagination/customPaginationStyles.js
@@ -6,6 +6,9 @@ import { makeStyles } from '@mui/styles'
 
 const useStyles = makeStyles((theme) => ({
   customPaginationRoot: {
+    width: '100vw',
+    display: 'flex',
+    justifyContent: 'center',
     '& .MuiPaginationItem-root': {
       backgroundColor: colors.cultured,
       color: colors.slateGray,
@@ -14,24 +17,24 @@ const useStyles = makeStyles((theme) => ({
         backgroundColor: colors.slateGray,
         color: colors.cultured,
       },
-      // [theme.breakpoints.down('xs')]: {
-      //   height: 30,
-      //   width: 30,
-      //   fontSize: 20,
-      //   margin: '0px 7.5px',
-      // },
-      // [theme.breakpoints.only('sm')]: {
-      //   height: 40,
-      //   width: 40,
-      //   fontSize: 24,
-      //   margin: '0px 10px',
-      // },
-      // [theme.breakpoints.up('md')]: {
-      //   height: 50,
-      //   width: 50,
-      //   fontSize: 24,
-      //   margin: '0px 12.5px',
-      // },
+      [theme.breakpoints.only('xs')]: {
+        height: 30,
+        width: 30,
+        fontSize: 20,
+        margin: '0px 7.5px',
+      },
+      [theme.breakpoints.only('sm')]: {
+        height: 40,
+        width: 40,
+        fontSize: 21,
+        margin: '0px 10px',
+      },
+      [theme.breakpoints.up('md')]: {
+        height: 50,
+        width: 50,
+        fontSize: 24,
+        margin: '0px 10px',
+      },
     },
     '& .Mui-selected': {
       backgroundColor: colors.orangeYellowCrayota,
@@ -46,18 +49,24 @@ const useStyles = makeStyles((theme) => ({
       display: 'flex',
       justifyContent: 'center',
       alignItems: 'center',
-      // [theme.breakpoints.down('xs')]: {
-      //   height: 30,
-      //   width: 30,
-      // },
-      // [theme.breakpoints.only('sm')]: {
-      //   height: 40,
-      //   width: 40,
-      // },
-      // [theme.breakpoints.up('md')]: {
-      //   height: 50,
-      //   width: 50,
-      // },
+      [theme.breakpoints.only('xs')]: {
+        height: 30,
+        width: 30,
+        fontSize: 20,
+        margin: '0px 7.5px',
+      },
+      [theme.breakpoints.only('sm')]: {
+        height: 40,
+        width: 40,
+        fontSize: 21,
+        margin: '0px 10px',
+      },
+      [theme.breakpoints.up('md')]: {
+        height: 50,
+        width: 50,
+        fontSize: 24,
+        margin: '0px 10px',
+      },
     },
   },
 }))
