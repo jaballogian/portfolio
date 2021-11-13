dde41f836b9ff0dded050f62552dd6fb9bb02218
jaballogian <jaballogianrma77@gmail.com>
2021-11-13 21:49:45 +0700

    add Bottom Content and Top Content components and their styles in Left Content component

--- src/pages/Home/Projects/LeftContent.jsx
+++ src/pages/Home/Projects/LeftContent.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-
-// COMPONENTS
-import TopContent from './TopContent'
-import BottomContent from './BottomContent'
+import { Link } from 'react-router-dom'
 
 // MATERIAL UI CORES
+import Button from '@mui/material/Button'
 import Grid from '@mui/material/Grid'
+import Typography from '@mui/material/Typography'
 
 // STYLES
 import useStyles from './leftContentUseStyles'
@@ -21,8 +20,68 @@ const LeftContent = (props) => {
       xs={12} lg={6}
     >
       <div className={classes.leftContentRoot}>
-        <TopContent content={content}/>
-        <BottomContent content={content}/>
+        <div className={classes.topContainer}>
+          {/* TITLE */}
+          <Typography
+            variant='h2'
+            className={classes.title}
+          >
+            {content.title}
+          </Typography>
+
+          {/* TYPE */}
+          <div className={classes.typeContainer}>
+            {content.type.map((item, index) => (
+              <div 
+                key={index}
+                className={classes.typeItem}
+              >
+                <Typography
+                  variant='h4'
+                  className={classes.typeText}
+                >
+                  {item}
+                </Typography>
+              </div>
+            ))}
+          </div>
+
+          {/* TECHNOLOGIES */}
+          <Typography
+            variant='h4'
+            className={classes.technologies}
+          >
+            {`Technologies: ${content.technologies}`}
+          </Typography>
+        </div>
+        
+        <div className={classes.bottomContentRoot}>
+          {/* DESCRIPTION */}
+          <Typography
+            variant='h4'
+            className={classes.description}
+          >
+            {content.description}
+          </Typography>
+          
+          {/* EXPLORE MORE */}
+          <Link 
+            to={content.link}
+            className={classes.linkText}
+          >
+            <Button 
+              className={classes.exploreMoreContainer} 
+              variant='contained'
+            >
+              <Typography
+                variant='h4'
+                className={classes.exploreMoreText}
+              >
+                Explore more
+              </Typography>
+            </Button>
+          </Link>
+        </div>
       </div>
     </Grid>
   )
--- src/pages/Home/Projects/leftContentUseStyles.js
+++ src/pages/Home/Projects/leftContentUseStyles.js
@@ -1,3 +1,6 @@
+// CONSTANTS
+import colors from 'constants/colors'
+
 // MATERIAL UI STYLES
 import { makeStyles } from '@mui/styles'
 
@@ -23,6 +26,180 @@ const useStyles = makeStyles((theme) => ({
     // [theme.breakpoints.up('xl')]: {
     //   paddingRight: 50,
     // },
+  },
+  topContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-end',
+    // [theme.breakpoints.down('xs')]: {
+    //   // height: '37.5vh',
+    //   minHeight: 300,
+    // },
+    // [theme.breakpoints.only('sm')]: {
+    //   // height: '37.5vh',
+    //   minHeight: 300,
+    // },
+    // [theme.breakpoints.only('md')]: {
+    //   // height: '37.5vh',
+    //   minHeight: 300,
+    // },
+    // [theme.breakpoints.only('lg')]: {
+    //   height: '50vh',
+    // },
+    // [theme.breakpoints.up('xl')]: {
+    //   height: '50vh',
+    // },
+  },
+  title: {
+    // [theme.breakpoints.down('sm')]: {
+    //   marginBottom: 16,
+    // },
+    // [theme.breakpoints.only('md')]: {
+    //   marginBottom: 24,
+    // },
+    // [theme.breakpoints.only('lg')]: {
+    //   marginBottom: 32,
+    // },
+    // [theme.breakpoints.up('xl')]: {
+    //   marginBottom: 40,
+    // },
+  },
+  typeContainer: {
+    display: 'flex',
+    // [theme.breakpoints.down('sm')]: {
+    //   marginBottom: 16,
+    // },
+    // [theme.breakpoints.only('md')]: {
+    //   marginBottom: 24,
+    // },
+    // [theme.breakpoints.only('lg')]: {
+    //   marginBottom: 32,
+    // },
+    // [theme.breakpoints.up('xl')]: {
+    //   marginBottom: 40,
+    // },
+  },
+  typeItem: {
+    backgroundColor: colors['carribeanGreen'],
+    borderRadius: 20,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    // [theme.breakpoints.down('xs')]: {
+    //   marginRight: 10,
+    //   width: 150,
+    //   height: 40,
+    // },
+    // [theme.breakpoints.only('sm')]: {
+    //   marginRight: 10,
+    //   width: 200,
+    //   height: 40,
+    // },
+    // [theme.breakpoints.only('md')]: {
+    //   marginRight: 15,
+    //   width: 225,
+    //   height: 50,
+    // },
+    // [theme.breakpoints.only('lg')]: {
+    //   marginRight: 20,
+    //   width: 225,
+    //   height: 50,
+    // },
+    // [theme.breakpoints.up('xl')]: {
+    //   marginRight: 25,
+    //   width: 225,
+    //   height: 50,
+    // },
+  },
+  typeText: {
+    color: 'white',
+  },
+  technologies: {
+    color: colors['slateGray'],
+    // [theme.breakpoints.down('xs')]: {
+    //   height: 90,
+    //   marginBottom: 16,
+    // },
+    // [theme.breakpoints.only('sm')]: {
+    //   height: 80,
+    //   marginBottom: 16,
+    // },
+    // [theme.breakpoints.only('md')]: {
+    //   marginBottom: 24,
+    // },
+    // [theme.breakpoints.only('lg')]: {
+    //   height: 80,
+    //   marginBottom: 32,
+    // },
+    // [theme.breakpoints.up('xl')]: {
+    //   height: 80,
+    //   marginBottom: 40,
+    // },
+  },
+  bottomContentRoot: {
+    // [theme.breakpoints.down('xs')]: {
+    //   // height: '37.5vh',
+    //   minHeight: 300,
+    // },
+    // [theme.breakpoints.only('sm')]: {
+    //   // height: '37.5vh',
+    //   minHeight: 300,
+    // },
+    // [theme.breakpoints.only('md')]: {
+    //   // height: '37.5vh',
+    //   minHeight: 300,
+    // },
+    // [theme.breakpoints.only('lg')]: {
+    //   height: '50vh',
+    // },
+    // [theme.breakpoints.up('xl')]: {
+    //   height: '50vh',
+    // },
+  },
+  description: {
+    colors: 'black',
+    // [theme.breakpoints.down('sm')]: {
+    //   marginBottom: 16,
+    //   height: 200,
+    // },
+    // [theme.breakpoints.only('md')]: {
+    //   marginBottom: 24,
+    //   height: 160,
+    // },
+    // [theme.breakpoints.only('lg')]: {
+    //   marginBottom: 32,
+    //   height: 160,
+    // },
+    // [theme.breakpoints.up('xl')]: {
+    //   marginBottom: 40,
+    //   height: 160,
+    // },
+  },
+  linkText: {
+    textDecoration: 'none',
+  },
+  exploreMoreContainer: {
+    backgroundColor: colors['paradisePink'],
+    textTransform: 'none',
+    borderRadius: 10,
+    '&:hover': {
+      backgroundColor: colors['redCrayola'],
+    },
+    // [theme.breakpoints.down('xs')]: {
+    //   height: 45,
+    //   width: 200,
+    // },
+    // [theme.breakpoints.only('sm')]: {
+    //   height: 50,
+    //   width: 225,
+    // },
+    // [theme.breakpoints.up('md')]: {
+    //   height: 60,
+    //   width: 260,
+    // },
+  },
+  exploreMoreText: {
+    color: 'white',
   },
 }))
 
