2090056072d3f7c2c809828a36ade34f2fcb2545
jaballogian <jaballogianrma77@gmail.com>
2021-11-18 20:15:57 +0700

    add certificates data

---
+++ src/pages/Home/Certificates/certificatesData.js
@@ -0,0 +1,33 @@
+const certificatesData = [
+  {
+    // RIGHT CONTENT
+    title: 'Certificate Title 1',
+    typeList: [
+      'Mobile App',
+      'IoT Device',
+    ],
+    providers: 'Provider 1 & Provider 2',
+    descriptions: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+  },
+  {
+    // RIGHT CONTENT
+    title: 'Certificate Title 2',
+    typeList: [
+      'Website',
+      'IoT Device',
+    ],
+    providers: 'Provider 1',
+    descriptions: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+  },
+  {
+    // RIGHT CONTENT
+    title: 'Certificate Title 3',
+    typeList: [
+      'IoT Device',
+    ],
+    providers: 'Provider 1 & Provider 2',
+    descriptions: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+  },
+]
+
+export default certificatesData
