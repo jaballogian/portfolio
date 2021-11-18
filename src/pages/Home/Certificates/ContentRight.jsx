ed092eef941774a4e7cd1760ee2c93272b6208fb
jaballogian <jaballogianrma77@gmail.com>
2021-11-18 20:28:30 +0700

    fill contents and their styles in Content Right component

--- src/pages/Home/Certificates/ContentRight.jsx
+++ src/pages/Home/Certificates/ContentRight.jsx
@@ -1,14 +1,54 @@
 import React from 'react'
 
+// MATERIAL UI CORES
+import Typography from '@mui/material/Typography'
+
 // STYLES
 import useStyles from './contentRightUseStyles'
 
-const ContentRight = () => {
+const ContentRight = (props) => {
+  const { content } = props
+
   const classes = useStyles()
 
   return (
     <div className={classes.contentRightRoot}>
-      Content Right
+      {/* TITLE */}
+      <Typography
+        variant='h3'
+        className={classes.title}
+      >
+        {content.title}
+      </Typography>
+
+      {/* TYPE */}
+      <div className={classes.typeContainer}>
+        {content.typeList.map((item, index) => (
+          <Typography
+            key={index}
+            variant='h5'
+            className={classes.typeItem}
+          >
+            {item}
+          </Typography>
+        ))}
+      </div>
+
+      {/* TECHNOLOGIES */}
+      <Typography
+        variant='h5'
+        className={classes.providers}
+      >
+        {`From: ${content.providers}`}
+      </Typography>
+    
+      {/* DESCRIPTION */}
+      <Typography
+        variant='h5'
+        className={classes.description}
+      >
+        {content.description}
+      </Typography>
     </div>
   )
 }
--- src/pages/Home/Certificates/contentRightUseStyles.js
+++ src/pages/Home/Certificates/contentRightUseStyles.js
@@ -1,8 +1,48 @@
+// CONSTANTS
+import colors from 'constants/colors'
+
 // MATERIAL UI STYLES
 import { makeStyles } from '@mui/styles'
 
 const useStyles = makeStyles((theme) => ({
   contentRightRoot: {
+    width: '50vw',
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    paddingRight: 100,
+    paddingLeft: 25,
+  },
+  title: {
+    marginBottom: 25,
+  },
+  typeContainer: {
+    display: 'flex',
+    marginBottom: 25,
+  },
+  typeItem: {
+    backgroundColor: colors.carribeanGreen,
+    borderRadius: 20,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 10,
+    width: 175,
+    height: 40,
+    color: 'white',
+    '&:last-child': {
+      marginRight: 0,
+    },
+  },
+  providers: {
+    color: colors.slateGray,
+    marginBottom: 40,
+  },
+  description: {
+    colors: 'black',
+    marginBottom: 40,
+    height: 175,
   },
 }))
 
