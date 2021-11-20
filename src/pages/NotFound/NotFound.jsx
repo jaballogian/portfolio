7de14dadecea014115bfa499b4237d16576c51e0
jaballogian <jaballogianrma77@gmail.com>
2021-11-20 17:48:46 +0700

    add Not Found page and its styles

---
+++ src/pages/NotFound/NotFound.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+// STYLES
+import useStyles from './notFoundUseStyles'
+
+const NotFound = () => {
+  const classes = useStyles()
+
+  return (
+    <div>
+      Not Found
+    </div>
+  )
+}
+
+export default NotFound
---
+++ src/pages/NotFound/notFoundUseStyles.js
@@ -0,0 +1,11 @@
+// MATERIAL UI STYLES
+import { makeStyles } from '@mui/styles'
+
+const useStyles = makeStyles((theme) => ({
+  notFoundRoot: {
+    width: '100%',
+    height: '100vh',
+  },
+}))
+
+export default useStyles
