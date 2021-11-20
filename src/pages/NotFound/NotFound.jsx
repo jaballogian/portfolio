c1b9b98ce5af5fce635eedc7eb06d82a3ee2fb99
jaballogian <jaballogianrma77@gmail.com>
2021-11-20 18:58:36 +0700

    fill contents and their styles in Not Found page

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,4 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+// IMAGES
+import IllustrationNotFound from 'assets/images/illustrations/not_found.svg'
+
+// MATERIAL UI CORES
+import Button from '@mui/material/Button'
+import Typography from '@mui/material/Typography'
 
 // STYLES
 import useStyles from './notFoundUseStyles'
@@ -7,8 +15,47 @@ const NotFound = () => {
   const classes = useStyles()
 
   return (
-    <div>
-      Not Found
+    <div className={classes.notFoundRoot}>
+      {/* CAPTION */}
+      <Typography
+        variant='h4'
+        className={classes.caption}
+      >
+        404 Not Found
+      </Typography>
+
+      {/* TITLE */}
+      <Typography
+        variant='h3'
+        className={classes.title}
+      >
+        Sorry, the page doesn't exist
+      </Typography>
+
+      {/* IMAGE */}
+      <img
+        src={IllustrationNotFound} 
+        alt='' 
+        className={classes.image}
+      />
+
+      {/* BUTTON */}
+      <Link 
+        to='/'
+        className={classes.linkText}
+      >
+        <Button 
+          className={classes.button} 
+          variant='contained'
+        >
+          <Typography
+            variant='h5'
+            className={classes.buttonText}
+          >
+            Back to Home
+          </Typography>
+        </Button>
+      </Link>
     </div>
   )
 }
--- src/pages/NotFound/notFoundUseStyles.js
+++ src/pages/NotFound/notFoundUseStyles.js
@@ -1,3 +1,6 @@
+// CONSTANTS
+import colors from 'constants/colors'
+
 // MATERIAL UI STYLES
 import { makeStyles } from '@mui/styles'
 
@@ -5,6 +8,30 @@ const useStyles = makeStyles((theme) => ({
   notFoundRoot: {
     width: '100%',
     height: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  caption: {
+    color: colors.cadetBlueCrayola,
+  },
+  title: {
+    color: 'black',
+  },
+  image: {},
+  linkText: {
+    textDecoration: 'none',
+  },
+  button: {
+    backgroundColor: colors.paradisePink,
+    textTransform: 'none',
+    borderRadius: 10,
+    '&:hover': {
+      backgroundColor: colors.redCrayola,
+    },
+  },
+  buttonText: {
+    color: 'white',
   },
 }))
 
