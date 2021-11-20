ee3ebf7e3bca47cb642d03e85223b07773a92c06
jaballogian <jaballogianrma77@gmail.com>
2021-11-20 17:36:41 +0700

    add box shadow style for Custom Pagination component

--- src/components/CustomPagination/customPaginationStyles.js
+++ src/components/CustomPagination/customPaginationStyles.js
@@ -14,6 +14,7 @@ const useStyles = makeStyles((theme) => ({
       backgroundColor: colors.cultured,
       color: colors.slateGray,
       borderRadius: '50%',
+      boxShadow: '0px 5px 10px rgba(0, 0, 0, 0.1)',
       '&:hover': {
         backgroundColor: colors.slateGray,
         color: colors.cultured,
